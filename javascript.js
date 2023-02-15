171e5dbe446949c12d1947ef35b5c577c25a6dd1
IngoMiranda <ingolima26@gmail.com>
2023-02-15 07:04:17 -0300

    continuação dos ajuste da primeira secção do projeto

--- javascript.js
+++ javascript.js
@@ -3,7 +3,15 @@ let hamburgue =  document.querySelector('.list');
 let ul = document.querySelector('ul');
 
 
-ul.style.dispçlay = "none";
-header.style.background = "red";
+
+
 hamburgue.style.background = "blue";
-hamburgue.innerHTML='<div> <img src = "./assets/images/icon-menu.svg"></div>';
+hamburgue.style.cursor.background = "pointer";
+
+
+function clickburgue(){
+
+
+}
+
+
