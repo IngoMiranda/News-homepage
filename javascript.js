0c44a9aecdafa8110eb816876f8da239bc0c3ddb
IngoMiranda <ingolima26@gmail.com>
2023-02-15 06:04:45 -0300

    ajuste mobile

--- javascript.js
+++ javascript.js
@@ -1,3 +1,9 @@
 let header = document.querySelector('#header');
+let hamburgue =  document.querySelector('.list');
+let ul = document.querySelector('ul');
 
+
+ul.style.dispçlay = "none";
 header.style.background = "red";
+hamburgue.style.background = "blue";
+hamburgue.innerHTML='<div> <img src = "./assets/images/icon-menu.svg"></div>';
