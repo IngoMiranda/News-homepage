3d94ca950fbee2e2afea26ef221f52e10365908d
IngoMiranda <ingolima26@gmail.com>
2023-02-16 07:20:17 -0300

    :}

--- javascript.js
+++ javascript.js
@@ -1,9 +1,11 @@
 
 let btn = document.querySelector(".btn img");
+let list = document.querySelector(".list");
 
 function clickburgue(){
    
-    btn.setAttribute("src", "./assets/images/icon-menu-close.svg")
+    btn.setAttribute("src", "./assets/images/icon-menu-close.svg");
+    list.style.display = "block"
 
 
 }
