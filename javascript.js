ee77a7a9526688b7b2f18662d5074aeb55ca7ced
IngoMiranda <ingolima26@gmail.com>
2023-02-17 20:20:33 -0300

    ajuste responsividade menu

--- javascript.js
+++ javascript.js
@@ -5,24 +5,10 @@ function clickburgue(){
 
     let btn = document.querySelector(".btn img");
     let list = document.querySelector(".menu-list");
-    let li = document.querySelectorAll("li");
-
-   
+       
     btn.classList.toggle("active");
     list.classList.toggle("active");
-
-    li.forEach((li, idex)=>{
-
-        li.style.background = "red"
-
-    });
-
-    
-    
-   
-    
-
-    
+ 
     if(btn.classList.contains("active")){
 
         btn.setAttribute("src", "./assets/images/icon-menu-close.svg");
