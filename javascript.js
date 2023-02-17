2019288841b321168f04ed3633482c258d69cab8
IngoMiranda <ingolima26@gmail.com>
2023-02-17 16:06:52 -0300

    ajustejs

--- javascript.js
+++ javascript.js
@@ -1,8 +1,10 @@
 
-let btn = document.querySelector(".btn img");
-let list = document.querySelector(".list");
+
 
 function clickburgue(){
+
+    let btn = document.querySelector(".btn img");
+    let list = document.querySelector(".list");
    
     btn.setAttribute("src", "./assets/images/icon-menu-close.svg");
     list.style.display = "block"
