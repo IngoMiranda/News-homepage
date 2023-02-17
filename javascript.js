9e1212e2f62b7cf84b564a869d989bd6a7ff91e3
IngoMiranda <ingolima26@gmail.com>
2023-02-17 18:32:02 -0300

    conclusão do menu mobile

--- javascript.js
+++ javascript.js
@@ -4,9 +4,35 @@
 function clickburgue(){
 
     let btn = document.querySelector(".btn img");
-    let list = document.querySelector(".list");
+    let list = document.querySelector(".menu-list");
+    let li = document.querySelectorAll("li");
+
    
-    btn.setAttribute("src", "./assets/images/icon-menu-close.svg");
+    btn.classList.toggle("active");
+    list.classList.toggle("active");
+
+    li.forEach((li, idex)=>{
+
+        li.style.background = "red"
+
+    });
+
+    
+    
+   
+    
+
+    
+    if(btn.classList.contains("active")){
+
+        btn.setAttribute("src", "./assets/images/icon-menu-close.svg");
+        
+    }else{
+        btn.setAttribute("src", "./assets/images/icon-menu.svg");
+    }
+
+    
+ 
     
 
 
