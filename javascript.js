32df72ad033f79404f28c2e56b60dee49e2d5062
IngoMiranda <ingolima26@gmail.com>
2023-02-18 12:55:54 -0300

    ajust

--- javascript.js
+++ javascript.js
@@ -1,28 +1,14 @@
+function clickburgue() {
+  let btn = document.querySelector(".btn img");
+  let list = document.querySelector(".menu-list");
 
+  btn.classList.toggle("active");
+  list.classList.toggle("active");
+  list.style.transition = "0.4s ease-in";
 
-
-function clickburgue(){
-
-    let btn = document.querySelector(".btn img");
-    let list = document.querySelector(".menu-list");
-       
-    btn.classList.toggle("active");
-    list.classList.toggle("active");
-    list.style.transition =  '0.4s ease-in'; 
- 
-    if(btn.classList.contains("active")){
-
-        btn.setAttribute("src", "./assets/images/icon-menu-close.svg");
-        
-    }else{
-        btn.setAttribute("src", "./assets/images/icon-menu.svg");
-    }
-
-    
- 
-    
-
-
+  if (btn.classList.contains("active")) {
+    btn.setAttribute("src", "./assets/images/icon-menu-close.svg");
+  } else {
+    btn.setAttribute("src", "./assets/images/icon-menu.svg");
+  }
 }
-
-
